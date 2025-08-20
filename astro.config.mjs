fbe67ecf39b5c5ae31fba714660b9aaf48dfd5bf
XxViRUsPRO <xxvirus.15@hotmail.com>
2025-08-20 16:58:52 +0300

    ref: updated the astro config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://chadinasser.dev",
   integrations: [
     mdx(),
     sitemap(),
